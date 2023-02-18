97f9d41e098028afb7ad3974431c4340e9cbd0f0
Aman Verma <aman@Amans-MacBook-Air.local>
2023-02-18 15:40:58 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Typewriter from "typewriter-effect";
 
 function App() {
+  const apiKey = "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";
+
+  const [searchTerm, setsearchTerm] = useState(" ");
+  const [value, setValue] = useState("");
+
+  const client = axios.create({
+    headers: {
+      Authorization: "Bearer " + apiKey,
+    },
+  });
+
+  const params = {
+    prompt: searchTerm,
+    model: "text-davinci-003",
+    max_tokens: 4000,
+    temperature: 0,
+  };
+
+  async function fetchData(term) {
+    const result = await client.post(
+      "https://api.openai.com/v1/completions",
+      term
+    );
+
+    console.log(result.data.choices[0].text);
+    setValue(result.data.choices[0].text);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Ask me anything ? "
+            onChange={(e) => {
+              setsearchTerm(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              fetchData(params);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="output">
+          <div className="value">
+            <Typewriter
+              options={{
+                pauseFor: 80000,
+                strings: [value],
+                autoStart: true,
+                delay: 20,
+              }}
+            />
+          </div>
+        </div>
+        <div className="copyright">
+          <p>
+            {/* {" "} */}
+            <p>
+              Copyright © {new Date().getFullYear()} All rights reserved | Made
+              by Aman{" "}
+            </p>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
